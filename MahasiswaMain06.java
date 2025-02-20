58c3f5f9c7c2d523f44580e92ed972a64573ee28
daffaputra09 <daffaputra863@gmail.com>
2025-02-20 20:56:14 +0700

    feat: MahasiswaMain06

---
+++ MahasiswaMain06.java
@@ -0,0 +1,53 @@
+class MahasiswaMain06{
+
+    String nama;
+    String nim;
+    String kelas;
+    Double ipk;
+
+    void tampilkanInformasi(){
+        System.out.println("Nama : "+ nama);
+        System.out.println("NIM : "+ nim);
+        System.out.println("IPK : "+ ipk);
+        System.out.println("Kelas : "+ kelas);
+    }
+
+    void ubahKelas(String kelasBaru){
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(Double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+            System.out.println("IPK berhasil diperbarui menjadi: " + ipk);
+        } else {
+            System.out.println("IPK tidak valid. Harus rentang 0.0 dan 4.0");
+        }
+    }
+
+    String nilaiKinerja(){
+        if (ipk >= 3.5){
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0){
+            return "Kinerja baik";
+        } else if (ipk >= 2.5){
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+
+    public static void main(String[] args) {  
+         
+        Mahasiswa06 mhs1 = new Mahasiswa06();  
+        mhs1.nama = "Muhammad Ali Farhan";  
+        mhs1.nim = "2241720117";  
+        mhs1.kelas = "SI 2J";  
+        mhs1.ipk = 3.55;  
+ 
+        mhs1.tampilkanInformasi();  
+        mhs1.ubahKelas("SI 2K");  
+        mhs1.updateIpk(3.60);  
+        mhs1.tampilkanInformasi();  
+    }
+}
